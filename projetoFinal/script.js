086c8846b3adb58e1aebb0a12acc76d3f9bec626
Aroldo12 <166452589+Aroldo12@users.noreply.github.com>
2024-07-02 12:17:16 -0300

    ProjetoFinal
    Grande Parte esta pronto

--- projetoFinal/script.js
+++ projetoFinal/script.js
@@ -1,6 +1,6 @@
 
-
-
+    const div = document.querySelector('.pagina')
+        div.style.display = 'none'
 function exibir(){
     const div = document.querySelector('.pagina')
 
@@ -13,3 +13,63 @@ function ocultar(){
 
     div.style.display = 'none'
 }
+
+function principal(eventoRecebido){
+    eventoRecebido?.preventDefault()
+
+    const personagem = document.querySelector('#id_personagem').value
+    const filme = document.querySelector('#id_filme').value
+    const ator = document.querySelector('#id_ator').value
+    const descricao = document.querySelector('#id_descricao').value
+
+    let lista = []
+    
+    localStorage.setItem('nome', personagem)
+    localStorage.setItem('Filme', filme)
+    localStorage.setItem('Ator', ator)
+    localStorage.setItem('Descrcao', descricao)
+
+    const teste = localStorage.getItem('nome')
+    const teste2 = localStorage.getItem('Filme')
+    const teste3 = localStorage.getItem('Ator')
+    const teste4 = localStorage.getItem('Descrcao')
+    
+// console.log(teste)
+// console.log(teste2)
+// console.log(teste3)
+// console.log(teste4)
+lista.push(teste)
+localStorage.setItem('nomes', JSON.stringify(lista) )
+
+
+   console.log(lista)
+
+    
+
+
+}
+let listaRecuperada = []
+// Só realizar o parse se houver dados no armazenamento local
+if(localStorage.getItem('nomes')){
+listaRecuperada = JSON.parse(localStorage.getItem('nomes'))
+}
+console.log(listaRecuperada)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+   
+    
+    
+
